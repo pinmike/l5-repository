b9529166d03463c6c049c96e2415abb211217af2
grahami <grahami@users.noreply.github.com>
2016-06-16 12:05:30 +0100

    Merge develop changes (#2)
    * Do validation when doing an updateWhere
    
    * changes for composer package support from github

--- src/Prettus/Repository/Eloquent/BaseRepository.php
+++ src/Prettus/Repository/Eloquent/BaseRepository.php
@@ -611,6 +611,15 @@ abstract class BaseRepository implements RepositoryInterface, RepositoryCriteria
      */
     public function updateWhere(array $attributes, array $where)
     {
+        if (!is_null($this->validator)) {
+            // we should pass data that has been casts by the model
+            // to make sure data type are same because validator may need to use
+            // this data to compare with data that fetch from database.
+            $attributes = $this->model->newInstance()->forceFill($attributes)->toArray();
+
+            $this->validator->with($attributes)->passesOrFail(ValidatorInterface::RULE_UPDATE);
+        }
+
         $tableName = $this->model->getTable();
         $connection = $this->model->getConnectionName();
         try {
